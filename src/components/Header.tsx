a7430ef6058275a6ab5b9cb713798ccadd09c694
minagishl <mvxd568.co@icloud.com>
2024-01-03 22:28:16 +0900

    Preparation correction that does not collapse when using a smartphone.

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,11 +5,18 @@ export default function Header() {
   return (
     <div className="mx-auto flex max-w-7xl items-center justify-between p-6 py-9 lg:px-8">
       <Link href="/">
-        <Image src="logo.svg" width="250" height="29" alt="避難所ウェブサービス" priority />
+        <Image
+          src="logo.svg"
+          width="250"
+          height="29"
+          alt="避難所ウェブサービス"
+          priority
+          className=" w-52 sm:w-60"
+        />
       </Link>
       <Link
         href="/register"
-        className="rounded-full bg-zinc-950 px-5 py-2 text-sm font-semibold text-zinc-50"
+        className="rounded-full bg-zinc-950 px-4 py-1 text-sm font-semibold text-zinc-50 sm:px-5 sm:py-2"
       >
         登録する
       </Link>
