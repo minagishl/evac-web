94c10fd2af6359c8bf5c708870e0ff6724612434
minagishl <mvxd568.co@icloud.com>
2024-01-03 22:17:14 +0900

    トップページのみ修正を加えました

--- src/pages/done.tsx
+++ src/pages/done.tsx
@@ -1,11 +1,11 @@
-import Header from "@/components/Header";
-import { Container } from "@mantine/core";
+import Header from '@/components/Header';
+import { Container } from '@mantine/core';
 
 export default function Done() {
   return (
     <>
       <Header />
-      <Container maw={"90%"} style={{ textAlign: "center" }} size="xs">
+      <Container maw={'90%'} style={{ textAlign: 'center' }} size="xs">
         <h1>登録が完了しました</h1>
       </Container>
     </>
