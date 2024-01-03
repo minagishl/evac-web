c45045796e1269f0e0e35e3884dea6fa8c708106
Kokohachi <99192596+Kokohachi@users.noreply.github.com>
2024-01-03 20:37:52 +0900

    GA

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,17 +1,47 @@
 // Import styles of packages that you've installed.
 // All packages except `@mantine/hooks` require styles imports
 import "@mantine/core/styles.css";
-
+import { useRouter } from "next/router";
+import { useEffect } from "react";
 import type { AppProps } from "next/app";
 import { createTheme, MantineProvider } from "@mantine/core";
+import * as gtag from "../../lib/gtag";
+import Script from "next/script";
 
 const theme = createTheme({
   /** Put your mantine theme override here */
 });
 
 export default function App({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+  useEffect(() => {
+    const handleRouterChange = (url: any) => {
+      gtag.pageview(url);
+    };
+    router.events.on("routeChangeComplete", handleRouterChange);
+    return () => {
+      router.events.off("routeChangeComplete", handleRouterChange);
+    };
+  }, [router.events]);
   return (
     <MantineProvider theme={theme}>
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_MEASUREMENT_ID}`}
+      />
+      <Script
+        id="gtag-init"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+           window.dataLayer = window.dataLayer || [];
+           function gtag(){dataLayer.push(arguments);}
+           gtag('js', new Date());
+
+           gtag('config', '${gtag.GA_MEASUREMENT_ID}');
+           `,
+        }}
+      />
       <Component {...pageProps} />
     </MantineProvider>
   );
